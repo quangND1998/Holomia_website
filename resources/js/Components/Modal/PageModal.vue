9102ee4db4fe519d2b5f936280a7d538719bc144
Nguyen Duc Quang <quangnd@VNIMATION.COM>
2022-07-20 11:22:48 +0700

    main

--- resources/js/Components/Modal/PageModal.vue
+++ resources/js/Components/Modal/PageModal.vue
@@ -56,7 +56,7 @@
                 </div>
                 <div class="p-3 row-span-2 col-span-2">
                   <div class="mb-4">
-                    <label>{{__('title')}} VietNamese </label>
+                    <label>{{__('title')}} VietNamese</label>
                     <input
                       type="text"
                       v-model="form.title_vn"
@@ -86,7 +86,7 @@
                 </div>
                 <div class="p-3">
                   <div class="mb-4">
-                    <label>{{__('description')}} VietNam</label>
+                    <label>{{__('description')}} VietNamese</label>
                     <textarea
                       type="text"
                       v-model="form.description_vn"
@@ -106,7 +106,6 @@
               >
                 <button
                   class="inline-block px-3 py-2 mr-1 mb-1 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                  
                   @click="closeModal()"
                 >{{ __('cancel') }}</button>
                 <button
