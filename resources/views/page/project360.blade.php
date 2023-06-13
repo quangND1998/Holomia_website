e17d19e78ee8d3ea0db90b1058d3f44c332d6411
Hoàng Thị Huyền <huyenht@VNIMATION.COM>
2023-06-13 09:02:00 +0700

    update type link and iframe

--- resources/views/page/project360.blade.php
+++ resources/views/page/project360.blade.php
@@ -61,7 +61,7 @@
                                      
                                     </div>
                                     @else
-                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/NLVRBz14yTY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                    <iframe width="560" height="315" src="{{$project->link}}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                                     @endif
                                 </div>
                             </div>
