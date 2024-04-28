cad715a63a321967e4bd043273ee60d711325930
ngant1096@gmail.com <ngant1096@gmail.com>
2024-04-28 14:00:23 +0700

    Edit

--- resources/views/template/camb/camb_news_noibat.blade.php
+++ resources/views/template/camb/camb_news_noibat.blade.php
@@ -5,7 +5,7 @@
             @if ($loop->index == 0)
                 <div  class=" lg:col-span-2 lg:row-span-2  relative ">
 
-                    <img class="h-full w-full object-cover" src="{{ $feature->image }}" alt="">
+                    <img class="h-[340px] lg:h-full w-full object-cover" src="{{ $feature->image }}" alt="">
                     <div
                         class="w-1/2 h-full  hover:opacity-[0.8] absolute top-0 bg-gradient-to-b from-[#1c1e5341] to-[#1C1E53] px-6 py-6 flex justify-end items-end">
                         <div class="py-4">
@@ -24,7 +24,7 @@
 
                 <div href="new/{{ __($feature->slug) }}" class="w-full group relative">
 
-                    <img class="h-full w-full object-cover" src="{{ $feature->image }}" alt="">
+                    <img class="h-[340px] lg:h-full  w-full object-cover" src="{{ $feature->image }}" alt="">
                     <div
                         class="group-hover:block hidden  transition-opacity  w-full h-full absolute top-0 bg-gradient-to-b from-[#1c1e5341] to-[#1C1E53] px-6 py-2 flex justify-end items-end">
                         <div class="py-4">
